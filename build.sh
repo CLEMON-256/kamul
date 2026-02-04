550d2b5142be07867e6093684bbfb3a0ac9fdba3
Yiga Junior <jnyxmacon@gmail.com>
2026-02-04 13:31:32 -0800

    first

---
+++ build.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+# Exit on error
+set -o errexit
+
+# Install dependencies
+pip install -r Backend/requirements.txt
+
+# Store current directory
+BASE_DIR=$(pwd)
+
+# Move to Django project directory
+cd Backend/restraunt
+
+# Convert static asset files
+python manage.py collectstatic --no-input
+
+# Apply any outstanding database migrations
+python manage.py migrate
+
+# Seed the database with initial data
+python seed_db.py
+
+# Update menu item images
+python update_menu_images.py
+
+# Return to base dir (optional, but good practice)
+cd $BASE_DIR
